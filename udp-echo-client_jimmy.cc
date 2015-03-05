0062c28b53bdc8b8c96e82fba2940b7df0bcf3be
jinmingmu <jinmingmu@gmail.com>
2015-03-04 16:15:59 -0800

    change the return type to ipv4 address

--- udp-echo-client_jimmy.cc
+++ udp-echo-client_jimmy.cc
@@ -10,21 +10,19 @@ idea_temp{
 				send x to neighbor
 				timeTemp = local_time()
 				Ptr<Packet> packet;
-				Address from;
-				while ((packet = socket->RecvFrom (from))):
+				while ((packet = socket->Recv() )):
 					if(local_time - timeTemp = 0.5 milsecond):
 				 		break
 				
 					if packet == ACK:
-						while ((packet = socket->RecvFrom (from))):
+						while ((packet = socket->Recv())):
 								if packet == solic:
 									send X
 								if packet == data and mesaage == x:
 									send ack
 		case null:
 			Ptr<Packet> packet;
-			Address from;
-			while ((packet = socket->RecvFrom (from))):
+			while ((packet = socket->RecvFrom() )):
 				if packet == data:
 					message = x
 					continue
@@ -38,13 +36,13 @@ idea_temp{
 	idea_temp();			
 }
 
-Ptr< NetDevice >
+int32_t
 findOneNeighbor(Node node_number){
 	Ptr< NetDevice >device = node_number.GetDevice(rand() % node_number.GetNDevices());
 	if (device != device->GetChannel()->GetDevice(0)){
-		return device->GetChannel()->GetDevice(0);
+		return GetInterfaceForDevice(device->GetChannel()->GetDevice(0)).GetAddress(1);
 	}else{
-		return device->GetChannel()->GetDevice(1);
+		return GetInterfaceForDevice(device->GetChannel()->GetDevice(1)).GetAddress(1);
 	}
 	
 //need to find who connect the device	
