b63e28f13978eb8dc4f65473c41cdb4d432e9973
Tingzhi Li <vincentltz@gmail.com>
2015-03-09 11:33:13 -0700

    [code]update results and update runAnalysis script

--- ns3/runAnalysis.sh
+++ ns3/runAnalysis.sh
@@ -4,12 +4,12 @@ python scratch/analysis.py scratch/maxtimecmd_250.txt scratch/maxhopscmd_250.txt
 python scratch/analysis.py scratch/maxtimecmd_400.txt scratch/maxhopscmd_250.txt > scratch/output_400.txt
 python scratch/analysis.py scratch/maxtimecmd_550.txt scratch/maxhopscmd_250.txt > scratch/output_550.txt
 python scratch/analysis.py scratch/maxtimecmd_700.txt scratch/maxhopscmd_250.txt > scratch/output_700.txt
-python scratch/analysis.py scratch/maxtimecmd_850.txt scratch/maxhopscmd_250.txt > scratch/output_850.txt
-python scratch/analysis.py scratch/maxtimecmd_1000.txt scratch/maxhopscmd_250.txt > scratch/output_1000.txt
-python scratch/analysis.py scratch/maxtimecmd_1150.txt scratch/maxhopscmd_250.txt > scratch/output_1150.txt
-python scratch/analysis.py scratch/maxtimecmd_1300.txt scratch/maxhopscmd_250.txt > scratch/output_1300.txt
-python scratch/analysis.py scratch/maxtimecmd_1450.txt scratch/maxhopscmd_250.txt > scratch/output_1450.txt
-python scratch/analysis.py scratch/maxtimecmd_1600.txt scratch/maxhopscmd_250.txt > scratch/output_1600.txt
+#python scratch/analysis.py scratch/maxtimecmd_850.txt scratch/maxhopscmd_250.txt > scratch/output_850.txt
+#python scratch/analysis.py scratch/maxtimecmd_1000.txt scratch/maxhopscmd_250.txt > scratch/output_1000.txt
+#python scratch/analysis.py scratch/maxtimecmd_1150.txt scratch/maxhopscmd_250.txt > scratch/output_1150.txt
+#python scratch/analysis.py scratch/maxtimecmd_1300.txt scratch/maxhopscmd_250.txt > scratch/output_1300.txt
+#python scratch/analysis.py scratch/maxtimecmd_1450.txt scratch/maxhopscmd_250.txt > scratch/output_1450.txt
+#python scratch/analysis.py scratch/maxtimecmd_1600.txt scratch/maxhopscmd_250.txt > scratch/output_1600.txt
 
 #example for 1 file output
 # rm output.txt #remove the old output or it will grow out of control
