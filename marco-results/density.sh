3aa7fff0eccd53e93469da68446a85cd4fec5c5d
Marco <falke.marco@gmail.com>
2015-03-12 23:27:10 -0700

    Off by one error...

--- marco-results/density.sh
+++ marco-results/density.sh
@@ -4,7 +4,7 @@ for num in ../random_topo_files/*; do
   nodes=0
   for file in `eval echo $num"/*"`; do
     echo -en "\r\e[K Parsing: "$file" (NodeCount: "$nodes" EdgeCount: "$edges")";
-    for i in $(seq 1 100); do
+    for i in {0..99}; do
       nodes=$((nodes + 1))
       count=$(cat $file | grep -E "(\("$i",|\s"$i"\))" | wc -l)
       edges=$((edges + count))
